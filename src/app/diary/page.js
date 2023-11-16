8c3fd2382d12717d3b9b96dcd3b1700f30475a85
afiqahdwi1@gmail.com <afiqahdwi1@gmail.com>
2023-11-16 19:52:34 +0800

    feat: membuat folder lebih rapi, membuat navbar, menambahkan fetching API

---
+++ src/app/diary/page.js
@@ -0,0 +1,50 @@
+'use client'
+import '@styles/diary.css'
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+export default function Diary() {
+    const [judul, setJudul] = useState([]);
+    const [isiDiary, setIsiDiary] = useState([]);
+
+    const endpointAPI = 'https://6555c0d784b36e3a431e3ecb.mockapi.io/diary';
+
+    async function getDiary() {
+        try{
+            const res = await axios.get(endpointAPI);
+            const data = res.data;
+
+            const judul = data.map((item) => item.judul);
+            setJudul(judul);
+
+            const isi_diary = data.map((item) => item.isi_diary);
+            setIsiDiary(isi_diary);
+        }
+        catch(error){
+            console.log("error data: ${error}")
+        }   
+    }
+
+    useEffect(() => {
+        getDiary();
+      }, []);
+
+    return(
+        <div>
+            {judul.length > 0 ? (
+                <ul>
+                    {judul.map((item, idx) => (
+                        <li>
+                            <div className='diary-container'>
+                                <h1>{judul[idx]}</h1>
+                                <p className='p-diary'>{isiDiary[idx]}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                "API not loading"
+            )}
+        </div>
+    )
+}
