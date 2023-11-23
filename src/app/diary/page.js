c06471f5bc3ca1f1490a3bebe78407699cd61bf2
afiqahdwi1@gmail.com <afiqahdwi1@gmail.com>
2023-11-23 14:26:15 +0800

    feat: menambahkan judul dan isi_diary

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -2,6 +2,7 @@
 import '@styles/diary.css'
 import { useEffect, useState } from "react"
 import axios from "axios"
+import Link from 'next/link';
 
 export default function Diary() {
     const [judul, setJudul] = useState([]);
@@ -34,12 +35,15 @@ export default function Diary() {
             {judul.length > 0 ? (
                 <ul>
                     {judul.map((item, idx) => (
-                        <li>
-                            <div className='diary-container'>
-                                <h1>{judul[idx]}</h1>
-                                <p className='p-diary'>{isiDiary[idx]}</p>
-                            </div>
-                        </li>
+                        <Link href={`/diary/${item}/${isiDiary[idx]}`}>
+                            <li key={idx}>
+                                <div className='diary-container'>
+                                    <h1>{judul[idx]}</h1>
+                                    <p className='p-diary'>{isiDiary[idx]}</p>
+                                </div>
+                            </li>
+                        </Link>
+        
                     ))}
                 </ul>
             ) : (
