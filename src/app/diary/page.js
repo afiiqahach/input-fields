e555f2dce2228e47be9eeaa9e03d3f431eb4b7aa
afiqahdwi1@gmail.com <afiqahdwi1@gmail.com>
2023-11-29 22:14:47 +0800

    feat: menambahkan add post method

--- src/app/diary/page.js
+++ src/app/diary/page.js
@@ -5,125 +5,140 @@ import axios from "axios"
 import Link from 'next/link';
 
 export default function Diary() {
-    const [input, setInput] = useState([])
-    const [nama, setNama] = useState ([])
-    const [empty, setEmpty] = useState (true)
+  const [getJudul, setGetJudul] = useState([])
+  const [getIsiDiary, setGetIsiDiary] = useState([])
+  const [getKoleksiData, setGetKoleksiData] = useState([])
 
-    const [judul, setJudul] = useState([]);
-    const [isiDiary, setIsiDiary] = useState([]);
-    const [isData, setData] = useState([])
+  const endpointAPI = 'https://6555c0d784b36e3a431e3ecb.mockapi.io/diary';
 
-    const endpointAPI = 'https://6555c0d784b36e3a431e3ecb.mockapi.io/diary';
 
-  // function button input
-  const handlerGantiNama = () => {
-    setNama(input);
-  }
+  async function getDiary() {
+    try {
+      const res = await axios.get(endpointAPI)
 
-  const handlerInputNama = (val) => {
-    setInput(val);
-    setEmpty(val.trim() === '');
-  }
+      // mengambil data
+      const data = res.data
+      setGetKoleksiData(data)
 
-  function enterButton(e){
-    if(e.code === "Enter") handlerGantiNama();
-  }
+      // mengambil judul
+      const judul = data.map((item) => item.judul)
+      setGetJudul(judul)
 
-  let content;
-  if (empty) {
-    content = (
-      <button className='cta' style={{
-        marginTop: '12px'
-      }}>
-        <p>DISABLED</p>
-      </button>
-    );
-  }else {
-    content = (
-      <button className='cta-button' style={{
-        marginTop: '12px'
-      }} onClick={()=> {
-        handlerGantiNama();
-      }}>
-        <p>Search</p>
-      </button>
-    )
-  }
-
-    async function getDiary() {
-        try{
-            const res = await axios.get(endpointAPI);
-            const data = res.data;
-
-            const judul = data.map((item) => item.judul);
-            setJudul(judul);
-
-            const isi_diary = data.map((item) => item.isi_diary);
-            setIsiDiary(isi_diary);
-        }
-        catch(error){
-            console.log("error fetching data: ${error}")
-        }   
+      // mengambil isi diary
+      const isi_diary = data.map((item) => item.isi_diary)
+      setGetIsiDiary(isi_diary)
+    } catch(error) {
+      console.error("error fetching data:", error)
     }
+}
 
-    useEffect(() => {
-        getDiary();
-      }, []);
+  const [postKetikJudul, setpostKetikJudul] = useState("")
+  const [postKetikIsi, setpostKetikIsi ] = useState("")
+
+  async function postDiary() {
+    try{
+      const res = await axios.post(endpointAPI, {
+        judul: postKetikJudul,
+        isi_diary: postKetikIsi
+      })
+      setGetKoleksiData([...getKoleksiData, res.data])
+
+      setpostKetikIsi("")
+      getDiary()
+    } catch (error) {
+      alert("gagal untuk POST API" + error)
+    }
+  }
+
+  // fungsi menginput judul dan isi diary dan enter button
+  function handlerInputJudul(event) {
+    event.preventDefault()
+    setpostKetikJudul(event.target.value)
+  }
+  function handlerInputIsi(event) {
+    event.preventDefault()
+    setpostKetikIsi(event.target.value)
+  }
+  function handlerSubmitDiary(event) {
+    postDiary()
+    setpostKetikJudul("")
+    setpostKetikIsi("")
+  }
+  function enterButton(e){
+    if(e.key === "Enter") {
+      handlerSubmitDiary();
+    }
+  }
+
+  useEffect(() => {
+    getDiary();
+  }, []);
 
     return(
         <div>
             <div className='banner-container'>
                 <div className='cta-banner-wrapper'>
                 {/* membuat button untuk input fields */}
-                <input className='input' 
-                    style={{marginTop: '12px'}} placeholder='search here..' 
-                    onInput={(val) => handlerInputNama(val.target.value)}
-                    onKeyDown={(value) => {
-                    enterButton(value)
-                    }}/>
-                {content}
+                <input 
+                  className='judul'
+                  type='text'
+                  style={{marginTop: '12px'}} placeholder='add another title..' 
+                  onChange={handlerInputJudul}
+                  onKeyDown={enterButton}
+                  value={postKetikJudul}
+                />
+
+                <input 
+                  className='isi-diary'
+                  type='text'
+                  style={{marginTop: '12px'}} placeholder='add another diary..' 
+                  onChange={handlerInputIsi}
+                  onKeyDown={enterButton}
+                  value={postKetikIsi}
+                />
+
+                
+                {postKetikJudul && postKetikJudul ? (
+                <div 
+                className='cta-button'
+                onClick={postDiary}>
+                  <p>Add New Diary</p>
+                </div>
+                ) : (
+                <div className='cta'
+                onClick={() => alert("Isi terlebih dahulu")}>
+                  <p>DISABLED</p>
+                </div>
+              )}
+                
+                
                 </div>
             </div>
 
-            {isData ? (judul.length > 0 ? (
-        <ul>
-          {judul.map((item, idx) => (
-            <Link href={`/diary/${item}/${isiDiary[idx]}`}>
-            
-            <li key={idx}>           
-              <div className={`diary-container ${idx === judul.length - 1? 'last-item' :''}`}>
-                <h1>{judul[idx]}</h1>
-                <p className="p-diary">{isiDiary[idx]}</p>
-              </div>
-            </li>
-            
-            </Link>
-          ))}
-        </ul>
+            {getKoleksiData ? (
+        getJudul.length > 0 ? (
+          <ul>
+            {getJudul.map((item, idx) => (
+              <Link href={`/diary/${item}/${getIsiDiary[idx]}`}>
+                <li key={idx}>
+                  <div
+                    className={`diary-container ${
+                      idx === getJudul.length - 1 ? "last-item" : ""
+                    }`}
+                  >
+                    <h1>{getJudul[idx]}</h1>
+                    <p className="p-diary">{getIsiDiary[idx]}</p>
+                  </div>
+                </li>
+              </Link>
+            ))}
+          </ul>
+        ) : (
+          "API loading.."
+        )
       ) : (
-        "API is loading..."
-      )
-      ) : 'API empty'
-
-      }
-            {/* {judul.length > 0 ? (
-                <ul>
-                    {judul.map((item, idx) => (
-                        <Link href={`/diary/${item}/${isiDiary[idx]}`}>
-                            <li key={idx}>
-                                <div className='diary-container'>
-                                    <h1>{judul[idx]}</h1>
-                                    <p className='p-diary'>{isiDiary[idx]}</p>
-                                </div>
-                            </li>
-                        </Link>
-        
-                    ))}
-                </ul>
-            ) : (
-                "API loading..."
-            )} */}
-     
-        </div>
+        "API empty"
+      )}
+      </div>
     )
 }
